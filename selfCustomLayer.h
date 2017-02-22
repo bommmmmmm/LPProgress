f288eda7c994911200114b83266026fa80e20fcd
bommmmmmm <liu5650342@163.com>
2017-02-22 17:30:12 +0800

    LPPregress
    一个自定义的圆点显示百分比的封装

---
+++ selfCustomLayer.h
@@ -0,0 +1,26 @@
+//
+//  selfCustomLayer.h
+//  454
+//
+//  Created by liuping on 2016/12/7.
+//  Copyright © 2016年 liuping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+@interface selfCustomLayer : UIView
+
+
+@property (nonatomic, assign) CGFloat count;
+
+//@property (nonatomic, strong) UIColor *taskColor;// 后面点的颜色；
+//
+//@property (nonatomic, strong) UIColor *processColor;// 实际进程点的颜色
+
+@property (nonatomic, assign) CGFloat width;
+
+@property (nonatomic, assign) CGFloat height;
+
+
+
+@end
---
+++ selfCustomLayer.m
@@ -0,0 +1,74 @@
+//
+//  selfCustomLayer.m
+//  454
+//
+//  Created by liuping on 2016/12/7.
+//  Copyright © 2016年 liuping. All rights reserved.
+//
+
+#import "selfCustomLayer.h"
+#define PI 3.14159265358979323846
+#define DEGREES_TO_RADIANS(angle) ((angle) / 180.0 * M_PI)
+
+@implementation selfCustomLayer
+
+
+
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+
+    }
+    return self;
+}
+
+- (void)drawRect:(CGRect)rect
+{
+
+    const CGFloat *components = CGColorGetComponents([MyManager shareInstance].progressColor.CGColor);
+    
+    // 确认中心点
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGFloat centX = CGRectGetMidX(self.bounds);
+    CGFloat centY = CGRectGetMidY(self.bounds);
+    CGContextTranslateCTM(context, centX, centY);
+    
+    for (int i = 0; i < 36; i ++) {
+                
+        CGContextSetRGBFillColor (context,  204/255.0, 204/255.0, 204/255.0, 1.0);//设置填充颜色
+        //画椭圆
+        CGContextAddEllipseInRect(context, CGRectMake(0, -((self.frame.size.height / 2) - 5) , self.width, self.height)); //椭圆
+        CGContextDrawPath(context, kCGPathFill);// 开始绘制
+       
+        CGContextSaveGState(context);// 保存上下文
+        CGContextRotateCTM(context, DEGREES_TO_RADIANS(10));// 旋转角度
+
+    }
+    
+    for (int i = 0; i < self.count; i ++) {
+        
+        CGContextSetRGBFillColor (context,  components[0], components[1], components[2], 1.0);//设置填充颜色
+        //画椭圆
+        CGContextAddEllipseInRect(context, CGRectMake(0, -((self.frame.size.height / 2) - 5), self.width, self.height)); //椭圆
+        CGContextDrawPath(context, kCGPathFill);// 开始绘制
+        
+        CGContextSaveGState(context);// 保存上下文
+        CGContextRotateCTM(context, DEGREES_TO_RADIANS(10));// 旋转角度
+        
+    }
+    
+    
+}
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
